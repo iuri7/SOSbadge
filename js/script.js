286a69fe7afb4911e386f127f6b970676720ed4c
iuri vespasiano <iuri@vescosolutions.com>
2020-11-08 18:13:48 -0500

    full layout complete add contact us form and fix pop up menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function naviToggle() {
+    var navi= document.getElementById("navi")
+    
+if (navi.style.display = "none") {
+    navi.style.display = "flex"} 
+else {
+    navi.style.display = "none"
+}
+}
+/*navi.style.display = "flex" ? navi.style.display = "none" : navi.style.display = "flex"*/
