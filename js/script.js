055fbcf35ae47948892d0a7ac01d73140502c3ed
iuri vespasiano <iuri@vescosolutions.com>
2020-11-15 14:52:56 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 function naviToggle() {
-    var navi= document.getElementById("navi")
-    
-if (navi.style.display = "none") {
-    navi.style.display = "flex"} 
-else {
-    navi.style.display = "none"
+        var navi = document.getElementById("navi")
+    console.log(navi.style.display)    
+       
+    if (navi.style.display === "flex") {
+        navi.style.display = "none"
+    }
+    else {
+        navi.style.display = "flex"
+    }
 }
 
 function contactbutton() {
-var contactbutton= document.getElementById("contactbutton") 
+    var contactbutton = document.getElementById("contactbutton")
 }
 function submitbutton() {
-var submitbutton= document.getElementById("submitbutton") 
-window.prompt("Thank you for interest on our SOS Badge. We will get back to you shortly")   
+    var submitbutton = document.getElementById("submitbutton")
+    window.prompt("Thank you for interest on our SOS Badge. We will get back to you shortly")
+    return (window.prompt)
+}
+
+function validateform() {
+    console.log("Hi")
+    var x = document.getElementById("submitbutton")
+    console.log(x)
+    if (x == "") {
+        alert("You must complete all fields ")
+        return false;
+    } else {
+        alert("Thank you for interest on our SOS Badge. We will get back to you shortly")
+        return;
+    }
 }
 
 /*navi.style.display = "flex" ? navi.style.display = "none" : navi.style.display = "flex"*/
