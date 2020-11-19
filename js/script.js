c7ac5d1ad34e73fb1de2fae3568785c68c1f2884
iuri vespasiano <iuri@vescosolutions.com>
2020-11-19 11:31:12 -0500

    added nth-child and updated Readme.MD

--- js/script.js
+++ js/script.js
@@ -10,14 +10,6 @@ function naviToggle() {
     }
 }
 
-function contactbutton() {
-    var contactbutton = document.getElementById("contactbutton")
-}
-function submitbutton() {
-    var submitbutton = document.getElementById("submitbutton")
-    window.prompt("Thank you for interest on our SOS Badge. We will get back to you shortly")
-    return (window.prompt)
-}
 
 function validateform() {
     console.log("Hi")
@@ -31,5 +23,8 @@ function validateform() {
         return;
     }
 }
+
+
+
 
 /*navi.style.display = "flex" ? navi.style.display = "none" : navi.style.display = "flex"*/
